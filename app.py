2c7b2fd518c79f31259d17d9c2dee708d23ac3d8
Sawyer Cherry <sawyeracherry@gmail.com>
2021-03-02 19:26:12 -0600

    Completed all challenges.

--- app.py
+++ app.py
@@ -91,12 +91,25 @@ def comparison_results():
     """Displays the relative weather for 2 different cities."""
     # TODO: Use 'request.args' to retrieve the cities & units from the query
     # parameters.
-    city1 = ''
-    city2 = ''
-    units = ''
+
+    city1 = request.args.get('city1')
+    city2 = request.args.get('city2')
+    units = request.args.get('units')
 
     # TODO: Make 2 API calls, one for each city. HINT: You may want to write a 
     # helper function for this!
+    def get_weather(city):
+        params = {
+            'appid': API_KEY,
+            'q': city,
+            'units': units
+        }
+        return requests.get(API_URL, params=params).json()
+
+
+
+    city1_json_call = get_weather(city1)
+    city2_json_call = get_weather(city2)
 
 
     # TODO: Pass the information for both cities in the context. Make sure to
@@ -104,7 +117,23 @@ def comparison_results():
     # HINT: It may be useful to create 2 new dictionaries, `city1_info` and 
     # `city2_info`, to organize the data.
     context = {
+        'date': datetime.now().strftime("%A, %B %-d, %Y"),
+        'units_letter': get_letter_for_units(units),
 
+        'city1_info': {
+            'city': city1_json_call['name'],
+            'temp': city1_json_call['main']['temp'],
+            'humidity': city1_json_call['main']['humidity'],
+            'wind_speed': city1_json_call['wind']['speed'],
+            'sunset': datetime.fromtimestamp(city1_json_call['sys']['sunset']).strftime("%-I:%M %p")
+        },
+        'city2_info': {
+            'city': city2_json_call['name'],
+            'temp': city2_json_call['main']['temp'],
+            'humidity': city2_json_call['main']['humidity'],
+            'wind_speed': city2_json_call['wind']['speed'],
+            'sunset': datetime.fromtimestamp(city2_json_call['sys']['sunset']).strftime("%-I:%M %p")
+        }
     }
 
     return render_template('comparison_results.html', **context)
